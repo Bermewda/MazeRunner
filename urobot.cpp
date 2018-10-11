98aeb300c49b69b3328e1f9ded629cd65c520bb5
Phunyawee Rodpoon <bermuda.mew@gmail.com>
2018-10-11 19:16:02 +0700

    car track wall 1way+2way+3way in 9x9

--- urobot.cpp
+++ urobot.cpp
@@ -1,40 +1,69 @@
 #include<stdio.h>
 
-int wallmem[7][7] = {{1,1,1, 1 ,1,1,1},
-					 {1,0,0, 0 ,0,0,1},
-					 {1,0,1, 1 ,1,0,1},
-					 {1,0,0, 0 ,1,0,1},
-					 {1,0,1, 1 ,1,0,1},
-					 {1,0,0, 0 ,0,0,1},
-					 {1,1,1, 1 ,1,1,1},
-					 }
+int wallhorimem[10][9] = 
+					 {{1,1,1,1, 1,1,1,1,1},   //0,0   0,1  
+		              {0,0,0,0, 0,0,0,0,0},   //1,0   
+		              {0,0,0,0, 0,0,0,0,0},
+					  {0,0,0,0, 0,0,0,0,0},
+					  {0,0,0,0, 0,0,0,0,0},
 
-int wallhorimem[4][3] = {{1,1,1},   //0,0   0,1   0,2
-		               {0,0,0},   //1,0   1,1   1,2
-		               {0,0,0},   //2,0   2,1   2,2
-		               {1,1,1}};   //3,0   3,1   3,2
+					  {0,0,0,0, 0,0,0,0,0},
+					  {0,0,0,0, 0,0,0,0,0},
+					  {0,0,0,0, 0,0,0,0,0},
+					  {0,0,0,0, 0,0,0,0,0},  
+		              {1,1,1,1, 1,1,1,1,1}};  
             
-int wallvertmem[3][4] = {{1,0,0,1},   //0,0   0,1   0,2   0,3
-		               {1,0,0,1},   //1,0   1,1   1,2   1,3
-		               {1,0,0,1}};   //2,0   2,1   2,2   2,3
-//  _ _ _
-// |  _  |
-// | | | |
-// |_ _|_| mappppp
-int wallhori[4][3] = {{1,1,1},   //0,0   0,1   0,2
-		              {0,1,0},   //1,0   1,1   1,2
-		              {0,1,0},   //2,0   2,1   2,2
-		              {1,1,1}};   //3,0   3,1   3,2
+int wallvertmem[9][10] = 
+					 {{1,0,0,0,0 , 0,0,0,0,1},   //0,0   0,1  
+		              {1,0,0,0,0 , 0,0,0,0,1},   //1,0   
+		              {1,0,0,0,0 , 0,0,0,0,1},
+					  {1,0,0,0,0 , 0,0,0,0,1},
+					  
+					  {1,0,0,0,0 , 0,0,0,0,1},
+					  {1,0,0,0,0 , 0,0,0,0,1},
+					  {1,0,0,0,0 , 0,0,0,0,1},
+					  {1,0,0,0,0 , 0,0,0,0,1},
+					  {1,0,0,0,0 , 0,0,0,0,1}};  
+
+
+int wallhori[10][9] = 
+					 {{1,1,1,1, 1,1,1,1,1},   //0,0   0,1  
+		              {0,0,1,0, 1,0,1,0,0},   //1,0   
+		              {0,0,1,0, 1,1,0,0,0},
+					  {0,0,0,0, 0,1,0,1,0},
+					  {0,0,1,0, 0,1,0,0,0},
+
+					  {0,0,0,1, 1,1,1,0,1},
+					  {0,0,1,1, 1,1,0,1,0},
+					  {0,0,0,0, 0,0,0,1,0},
+					  {0,0,0,0, 0,0,0,1,0},  
+		              {1,1,1,1, 1,1,1,1,1}};    
             
-int wallvert[3][4] = {{1,0,0,1},   //0,0   0,1   0,2   0,3
-		              {1,0,1,1},   //1,0   1,1   1,2   1,3
-		              {1,0,0,1}};   //2,0   2,1   2,2   2,3
+int wallvert[9][10] = 
+					 {{1,0,0,1,0 , 0,1,0,0,1},   //0,0   0,1  
+		              {1,0,1,0,1 , 0,0,1,0,1},   //1,0   
+		              {1,0,0,1,0 , 0,0,1,0,1},
+					  {1,0,0,1,1 , 1,1,0,1,1},
+
+					  {1,0,1,0,1 , 0,0,1,1,1},
+					  {1,0,1,0,0 , 0,0,0,0,1},
+					  {1,0,0,0,0 , 0,1,1,1,1},
+					  {1,0,0,0,0 , 0,1,1,1,1},
+					  {1,0,0,0,0 , 0,1,0,0,1}};
             
-int car[3][3] = {{2,1,2},   //0,0   0,1   0,2
-		        {1,0,1},   //1,0   1,1   1,2*
-		        {2,1,2}};   //2,0   2,1   2,2
-int x=2,y=2;
-int endx=1,endy=1;
+int car[9][9] = 
+			   {{6,5,4,3,4,5,6,7,8},   //0,0   0,1
+		        {5,4,3,4,3,4,5,6,7},   //1,0 
+				{4,3,2,1,2,3,4,5,6},
+				{3,2,1,0,1,2,3,4,5},
+				{4,3,2,1,2,3,4,5,6},
+				{5,4,3,2,3,4,5,6,7},
+				{6,5,4,3,4,5,6,7,8},
+				{7,6,5,4,5,6,7,8,9},
+				{8,7,6,5,6,7,8,9,10}};   
+int x=8,y=8;
+
+int endx=3,endy=3;
 int pattern = 0;
 int direction = 0;
 //#front=1 left=2  right=4  front+left+right=7
@@ -62,85 +91,112 @@ int main(){
 }
 
 void printwall(){
-	printf("wallhori\n");
-	for(int wallx=0;wallx<4;wallx++){
-		for(int wally=0;wally<3;wally++){
-		printf("%d ",wallhorimem[wallx][wally]);
+	printf("wallmap\n");
+	printf(" _ _ _ _ _ _ _ _ _\n");
+	for(int wallx=0;wallx<9;wallx++){
+		printf("|");
+		for(int wally=0;wally<9;wally++){
+			if(wallhori[wallx+1][wally]==1){
+				printf("_");
+			}else{
+				printf(" ");
+			}
+			//printf("%d ",wallhorimem[wallx][wally]);
+			if(wallvert[wallx][wally+1]==1){
+				printf("|");
+			}else{
+				printf(" ");
+			}
+		}
+		printf("\n");
 	}
-	printf("\n");
+	printf("wall\n");
+	printf(" _ _ _ _ _ _ _ _ _\n");
+	for(int wallx=0;wallx<9;wallx++){
+		printf("|");
+		for(int wally=0;wally<9;wally++){
+			if(wallhorimem[wallx+1][wally]==1){
+				printf("_");
+			}else{
+				printf(" ");
+			}
+			//printf("%d ",wallhorimem[wallx][wally]);
+			if(wallvertmem[wallx][wally+1]==1){
+				printf("|");
+			}else{
+				printf(" ");
+			}
+		}
+		printf("\n");
 	}
-	printf("wallvert\n");
-	for(int wallx=0;wallx<3;wallx++){
-		for(int wally=0;wally<4;wally++){
-		printf("%d ",wallvertmem[wallx][wally]);
-	}
-	printf("\n");
-	}
+	
+
+	
 }
 
 void checkdirection(){
 	switch(direction){
-		case 0 : printf("0 : ");//north
+		case 0 : //north
 				if(wallhori[x][y] == 1) {
-					printf("have a front wall\n");
+					printf("0 : have a front wall\n");
 					wallhorimem[x][y] = 1;
 					pattern+=1 ;   //#front
 				}
 				if(wallvert[x][y] == 1){
-					printf("have a left wall\n");
+					printf("0 : have a left wall\n");
 					wallvertmem[x][y] = 1;
 					pattern+=2 ;   //#left
 				}
 				if(wallvert[x][y+1] == 1){
-					printf("have a right wall\n");
+					printf("0 : have a right wall\n");
 					wallvertmem[x][y+1] = 1;
 					pattern+=4 ;   //#right 
 				} break;
-		case 1 : printf("1 : ");//east
+		case 1 : //east
 				if(wallvert[x][y] == 1) {
-					printf("have a front wall\n");
+					printf("1 : have a front wall\n");
 					wallvertmem[x][y] = 1;
 					pattern+=1 ;   //#front
 				}
 				if(wallhori[x+1][y] == 1){
-					printf("have a left wall\n");
+					printf("1 : have a left wall\n");
 					wallhorimem[x+1][y] = 1;
 					pattern+=2 ;   //#left
 				}
 				if(wallhori[x][y] == 1){
-					printf("have a right wall\n");
+					printf("1 : have a right wall\n");
 					wallhorimem[x][y] = 1;
 					pattern+=4 ;   //#right 
 				} break;
-		case 2 : printf("2 : ");//west
+		case 2 : //west
 				if(wallvert[x][y+1] == 1) {
-					printf("have a front wall\n");
+					printf("2 : have a front wall\n");
 					wallvertmem[x][y+1] = 1;
 					pattern+=1 ;   //#front
 				}
 				if(wallhori[x][y] == 1){
-					printf("have a left wall\n");
+					printf("2 : have a left wall\n");
 					wallhorimem[x][y] = 1;
 					pattern+=2 ;   //#left
 				}
 				if(wallhori[x+1][y] == 1){
-					printf("have a right wall\n");
+					printf("2 : have a right wall\n");
 					wallhorimem[x+1][y] = 1;
 					pattern+=4 ;   //#right 
 				} break;
-		default	: printf("3 : ");//south
+		default	: //south
 				if(wallhori[x+1][y] == 1) {
-					printf("have a front wall\n");
+					printf("3 : have a front wall\n");
 					wallhorimem[x][y] = 1;
 					pattern+=1 ;   //#front
 				}
 				if(wallvert[x][y+1] == 1){
-					printf("have a left wall\n");
+					printf("3 : have a left wall\n");
 					wallvertmem[x][y+1] = 1;
 					pattern+=2 ;   //#left
 				}
 				if(wallvert[x][y] == 1){
-					printf("have a right wall\n");
+					printf("3 : have a right wall\n");
 					wallvertmem[x][y] = 1;
 					pattern+=4 ;   //#right 
 				} break;	
@@ -206,55 +262,103 @@ void min2way(int a,int b){
 	}
 }
 
+void min3way(int a ,int b,int c){ //front,left,right
+	if(a!=b && a!=c && b!=c){ 
+		if(a<b && a<c){//go front
+			switch(direction){
+				case 0: printf("0 : ");//north
+						printf("go front\n"); 		x--;	direction = 0;	break;
+				case 1: printf("1 : ");//east
+						printf("go front\n"); 		y--;	direction = 1; 	break;
+				case 2: printf("2 : ");//west
+						printf("go front\n"); 		y++;	direction = 2; 	break;
+				default: printf("3 : ");//south
+						printf("go front\n"); 		x++;	direction = 3; 	break;
+			}
+		}else if(b<a && b<c){ //go left
+			switch(direction){
+				case 0: printf("0 : ");//north
+						printf("go left\n"); 		y--;	direction = 1;	break;
+				case 1: printf("1 : ");//east
+						printf("go left\n"); 		x++;	direction = 3; 	break;
+				case 2: printf("2 : ");//west
+						printf("go left\n"); 		x--;	direction = 0; 	break;
+				default: printf("3 : ");//south
+						printf("go left\n"); 		y++;	direction = 2; 	break;
+			}
+		}else{ //go right
+			switch(direction){
+				case 0: printf("0 : ");//north
+						printf("go right\n"); 		y++;	direction = 2;	break;
+				case 1: printf("1 : ");//east
+						printf("go right\n"); 		x--;	direction = 0; 	break;
+				case 2: printf("2 : ");//west
+						printf("go right\n"); 		x++;	direction = 3; 	break;
+				default: printf("3 : ");//south
+						printf("go right\n"); 		y--;	direction = 1; 	break;
+			}
+		}
+	}
+	else{
+		switch(direction){
+			case 0: printf("0 : ");//north
+					printf("go front\n"); 		x--;	direction = 0;	break;
+			case 1: printf("1 : ");//east
+					printf("go front\n"); 		y--;	direction = 1; 	break;
+			case 2: printf("2 : ");//west
+					printf("go front\n"); 		y++;	direction = 2; 	break;
+			default: printf("3 : ");//south
+					printf("go front\n"); 		x++;	direction = 3; 	break;
+		}
+
+	}
+}
+
 void checkpatndi(){
 	switch(direction){
-		case 0: printf("0 : ");//direction = 0
+		case 0: printf("0 : ");//north
 			switch(pattern){
-				case 0: printf("No wall : 3way\n");
-				
-				case 1: printf("left or right : 2way\n");		min2way(car[x][y-1],car[x][y+1]);break; 
-				case 2: printf("front or right : 2way\n");		min2way(car[x-1][y],car[x][y+1]);break;						
-				case 4: printf("front or left : 2way\n");		min2way(car[x-1][y],car[x][y-1]);break;
-				case 3: printf("go right\n"); 					y++;	direction = 2;break;//front+left
-				case 5: printf("go left\n"); 					y--;	direction = 1;break;//front+right
-				case 6: printf("go front\n"); 					x--;	direction = 0;break;//left+right
-				default:  printf("U turn\n"); 					x++;	direction = 3;break;//front+left+right
+				case 0: printf("No wall : 3way\n");			min3way(car[x-1][y],car[x][y-1],car[x][y+1]);break;				
+				case 1: printf("left or right : 2way\n");	min2way(car[x][y-1],car[x][y+1]);break; 
+				case 2: printf("front or right : 2way\n");	min2way(car[x-1][y],car[x][y+1]);break;						
+				case 4: printf("front or left : 2way\n");	min2way(car[x-1][y],car[x][y-1]);break;
+				case 3: printf("go right\n"); 				y++;	direction = 2;break;//front+left
+				case 5: printf("go left\n"); 				y--;	direction = 1;break;//front+right
+				case 6: printf("go front\n"); 				x--;	direction = 0;break;//left+right
+				default:  printf("U turn\n"); 				x++;	direction = 3;break;//front+left+right
 			}break;
-		case 1:	printf("1 : ");//direction = 1
+		case 1:	printf("1 : ");//east
 			switch(pattern){
-				case 0: printf("No wall : 3way\n");
-				
-				case 1: printf("left or right : 2way\n");		min2way(car[x+1][y],car[x-1][y]);break; 
-				case 2: printf("front or right : 2way\n");		min2way(car[x][y-1],car[x-1][y]);break;						
-				case 4: printf("front or left : 2way\n");		min2way(car[x][y-1],car[x+1][y]);break;
-				case 3: printf("go right\n"); 					x--;	direction = 0; 	break;//front+left 
-				case 5: printf("go left\n"); 					x++;	direction = 3; 	break;//front+right
-				case 6: printf("go front\n"); 					y--;	direction = 1; 	break;//left+right
-				default:  printf("U turn\n"); 					y++;	direction = 2; 	break;//front+left+right
+				case 0: printf("No wall : 3way\n");			min3way(car[x][y-1],car[x+1][y],car[x-1][y]);break;				
+				case 1: printf("left or right : 2way\n");	min2way(car[x+1][y],car[x-1][y]);break; 
+				case 2: printf("front or right : 2way\n");	min2way(car[x][y-1],car[x-1][y]);break;						
+				case 4: printf("front or left : 2way\n");	min2way(car[x][y-1],car[x+1][y]);break;
+				case 3: printf("go right\n"); 				x--;	direction = 0; 	break;//front+left 
+				case 5: printf("go left\n"); 				x++;	direction = 3; 	break;//front+right
+				case 6: printf("go front\n"); 				y--;	direction = 1; 	break;//left+right
+				default:  printf("U turn\n"); 				y++;	direction = 2; 	break;//front+left+right
 			}break;
-		case 2:	printf("2 : ");//direction = 2
+		case 2:	printf("2 : ");//west
 			switch(pattern){
-				case 0: printf("No wall : 3way\n");
-				
-				case 1: printf("left or right : 2way\n");		min2way(car[x-1][y],car[x+1][y]);break; 
-				case 2: printf("front or right : 2way\n");		min2way(car[x][y+1],car[x+1][y]);break;						
-				case 4: printf("front or left : 2way\n");		min2way(car[x][y+1],car[x-1][y]);break;
-				case 3: printf("go right\n"); 					x++;	direction = 3; 	break;//front+left
-				case 5: printf("go left\n"); 					x--;	direction = 0; 	break;//front+right
-				case 6: printf("go front\n"); 					y++;	direction = 2; 	break;//left+right
-				default:  printf("U turn\n"); 					y--;	direction = 1; 	break;//front+left+right
+				case 0: printf("No wall : 3way\n");			min3way(car[x][y+1],car[x-1][y],car[x+1][y]);break;				
+				case 1: printf("left or right : 2way\n");	min2way(car[x-1][y],car[x+1][y]);break; 
+				case 2: printf("front or right : 2way\n");	min2way(car[x][y+1],car[x+1][y]);break;						
+				case 4: printf("front or left : 2way\n");	min2way(car[x][y+1],car[x-1][y]);break;
+				case 3: printf("go right\n"); 				x++;	direction = 3; 	break;//front+left
+				case 5: printf("go left\n"); 				x--;	direction = 0; 	break;//front+right
+				case 6: printf("go front\n"); 				y++;	direction = 2; 	break;//left+right
+				default:  printf("U turn\n"); 				y--;	direction = 1; 	break;//front+left+right
 			}break;
-		default: printf("3 : ");//direction = 3
+		default: printf("3 : ");//south
 			switch(pattern){
-				case 0: printf("No wall : 3way\n");
-				
-				case 1: printf("left or right : 2way\n");		min2way(car[x][y+1],car[x][y-1]);break; 
-				case 2: printf("front or right : 2way\n");		min2way(car[x+1][y],car[x][y-1]);break;						
-				case 4: printf("front or left : 2way\n");		min2way(car[x+1][y],car[x][y+1]);break;
-				case 3: printf("go right\n"); 					y--;	direction = 1; 	break;//front+left
-				case 5: printf("go left\n"); 					y++;	direction = 2; 	break;//front+right
-				case 6: printf("go front\n"); 					x++;	direction = 3; 	break;//left+right
-				default:  printf("U turn\n"); 					x--;	direction = 0; 	break;//front+left+right
+				case 0: printf("No wall : 3way\n");			min3way(car[x+1][y],car[x][y+1],car[x][y-1]);break;				
+				case 1: printf("left or right : 2way\n");	min2way(car[x][y+1],car[x][y-1]);break; 
+				case 2: printf("front or right : 2way\n");	min2way(car[x+1][y],car[x][y-1]);break;						
+				case 4: printf("front or left : 2way\n");	min2way(car[x+1][y],car[x][y+1]);break;
+				case 3: printf("go right\n"); 				y--;	direction = 1; 	break;//front+left
+				case 5: printf("go left\n"); 				y++;	direction = 2; 	break;//front+right
+				case 6: printf("go front\n"); 				x++;	direction = 3; 	break;//left+right
+				default:  printf("U turn\n"); 				x--;	direction = 0; 	break;//front+left+right
 			}break;
 	}
 }
